0978280aa36c7dbd97a05b081f0be41be65905c2
Nabil <nabil.boukhorissa@edu.esiee.fr>
2025-03-19 12:16:24 +0100

    update Ex 2

--- TDS/src/TD3/main.cpp
+++ TDS/src/TD3/main.cpp
@@ -16,45 +16,56 @@ void print_vector(const std::vector<float>& vec) {
     std::cout << std::endl;
 }
 
-
-
 int main() {
+     //Ex1
+     std::vector<int> vec1 = {11, 12, 22, 25, 34, 64, 90}; // déjà trié (permet de voir la différence entre tri à bulle et le tri par sélection et dans quel cas l'un est plus intéressant que l'autre)
+     // std::vector<int> vec2 = {64, 34, 25, 12, 22, 11, 90};   // A tester avec ce tableau pour voir la diff
+     std::vector<int> vec2 = vec1;  
 
-    //Ex1
-    std::vector<int> vec1 = {11, 12, 22, 25, 34, 64, 90}; // déjà trié (permet de voir la différence entre tri à bulle et le tri par sélection et dans quel cas l'un est plus intéressant que l'autre)
-    // std::vector<int> vec2 = {64, 34, 25, 12, 22, 11, 90};   // A tester avec ce tableau pour voir la diff
-    std::vector<int> vec2 = vec1; 
+    std::cout << std::endl;
+    std::cout << "Exercice 1 : " << std::endl;
 
-    std::cout <<std::endl;
-    std::cout << "Exercice 1 : " <<std::endl;
+    std::cout << std::endl;
+
     std::cout << "Tableau : ";
     print_vector(vec1);
+
+    std::cout << std::endl;
 
     bubble_sort(vec1);
     std::cout << "Tri a bulle : ";
     print_vector(vec1);
 
+    std::cout << std::endl;
+
     selection_sort(vec2);
     std::cout << "Tri par selection : ";
     print_vector(vec2);
-    std::cout <<std::endl;
-
+    std::cout << std::endl;
 
     // Ex2
-    std::cout << "Exercice 2 : " <<std::endl;
+    std::cout << "Exercice 2 : " << std::endl;
+    std::cout << std::endl;
+
     std::vector<float> vec3 = {64.0, 34.0, 25.0, 12.0, 22.0, 11.0, 90.0};
     std::vector<float> vec4 = vec3;
 
     std::cout << "Tableau (float) : ";
     print_vector(vec3);
 
+    std::cout << std::endl;
+
     quick_sort(vec3);
     std::cout << "Tri rapide : ";
     print_vector(vec3);
+    
+    std::cout << std::endl;
 
     merge_sort(vec4);
     std::cout << "Tri fusion : ";
     print_vector(vec4);
+
+    std::cout << std::endl;
 
     return 0;
 }
--- TDS/src/TD3/tri.cpp
+++ TDS/src/TD3/tri.cpp
@@ -19,7 +19,7 @@ void bubble_sort(std::vector<int> &vec) {
             break;
         }
     }
-    std::cout << "Bubble sort iterations: " << iterations << std::endl;
+    std::cout << "Nb iterations tri a bulle : " << iterations << std::endl;
 }
 
 // Selection Sort
@@ -37,15 +37,16 @@ void selection_sort(std::vector<int> &vec) {
             std::swap(vec[i], vec[min_index]);
         }
     }
-    std::cout << "Selection sort iterations: " << iterations << std::endl;
+    std::cout << "Nb iterations tri par selection : " << iterations << std::endl;
 }
 
 // Quick Sort Partition
-size_t quick_sort_partition(std::vector<float> &vec, size_t left, size_t right, size_t const pivot) {
+size_t quick_sort_partition(std::vector<float> &vec, size_t left, size_t right, size_t const pivot, int &iterations) {
     float pivot_value = vec[pivot];
     std::swap(vec[pivot], vec[right]);
     size_t store_index = left;
     for (size_t i = left; i < right; ++i) {
+        ++iterations;
         if (vec[i] < pivot_value) {
             std::swap(vec[i], vec[store_index]);
             ++store_index;
@@ -56,30 +57,33 @@ size_t quick_sort_partition(std::vector<float> &vec, size_t left, size_t right, 
 }
 
 // Quick Sort
-void quick_sort(std::vector<float> &vec, size_t const left, size_t const right) {
+void quick_sort(std::vector<float> &vec, size_t const left, size_t const right, int &iterations) {
     if (left < right) {
         size_t pivot = left + (right - left) / 2;
-        size_t new_pivot = quick_sort_partition(vec, left, right, pivot);
+        size_t new_pivot = quick_sort_partition(vec, left, right, pivot, iterations);
         if (new_pivot > 0) {
-            quick_sort(vec, left, new_pivot - 1);
+            quick_sort(vec, left, new_pivot - 1, iterations);
         }
-        quick_sort(vec, new_pivot + 1, right);
+        quick_sort(vec, new_pivot + 1, right, iterations);
     }
 }
 
 void quick_sort(std::vector<float> &vec) {
+    int iterations = 0;
     if (!vec.empty()) {
-        quick_sort(vec, 0, vec.size() - 1);
+        quick_sort(vec, 0, vec.size() - 1, iterations);
     }
+    std::cout << "Nb iterations Tri rapide : " << iterations << std::endl;
 }
 
 // Merge Sort Merge
-void merge_sort_merge(std::vector<float> &vec, size_t const left, size_t const middle, size_t const right) {
+void merge_sort_merge(std::vector<float> &vec, size_t const left, size_t const middle, size_t const right, int &iterations) {
     std::vector<float> left_vec(vec.begin() + left, vec.begin() + middle + 1);
     std::vector<float> right_vec(vec.begin() + middle + 1, vec.begin() + right + 1);
 
     size_t left_index = 0, right_index = 0, merge_index = left;
     while (left_index < left_vec.size() && right_index < right_vec.size()) {
+        ++iterations;
         if (left_vec[left_index] <= right_vec[right_index]) {
             vec[merge_index++] = left_vec[left_index++];
         } else {
@@ -97,17 +101,19 @@ void merge_sort_merge(std::vector<float> &vec, size_t const left, size_t const m
 }
 
 // Merge Sort
-void merge_sort(std::vector<float> &vec, size_t const left, size_t const right) {
+void merge_sort(std::vector<float> &vec, size_t const left, size_t const right, int &iterations) {
     if (left < right) {
         size_t middle = left + (right - left) / 2;
-        merge_sort(vec, left, middle);
-        merge_sort(vec, middle + 1, right);
-        merge_sort_merge(vec, left, middle, right);
+        merge_sort(vec, left, middle, iterations);
+        merge_sort(vec, middle + 1, right, iterations);
+        merge_sort_merge(vec, left, middle, right, iterations);
     }
 }
 
 void merge_sort(std::vector<float> &vec) {
+    int iterations = 0;
     if (!vec.empty()) {
-        merge_sort(vec, 0, vec.size() - 1);
+        merge_sort(vec, 0, vec.size() - 1, iterations);
     }
+    std::cout << "Nb iterations Tri fusion : " << iterations << std::endl;
 }
